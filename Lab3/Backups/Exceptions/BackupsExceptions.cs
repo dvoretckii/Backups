659ccf881820d7065b944e38e809d3bd1528d56f
dvoretckii <pisosman1488@gmail.com>
2022-11-26 21:48:57 +0300

    Added SplitStorage.cs, Exceptions

---
+++ Lab3/Backups/Exceptions/BackupsExceptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Backups.Exceptions;
+
+public class BackupsException : Exception
+{
+    private BackupsException(string message)
+        : base(message) { }
+
+    public static BackupsException ElementNotFound()
+    {
+        return new BackupsException("The element was not found");
+    }
+
+    public static BackupsException NullableVariable()
+    {
+        return new BackupsException("The result is null");
+    }
+}
